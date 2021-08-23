d3e5b68f9fca07be99d4b950bf95174d536697ad
Daniella Noren <daniellanoren@live.se>
2021-08-23 17:01:10 +0200

    Create all empty classes

---
+++ PeopleMVC/Data/Entities/ViewModels/PersonViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleMVC.Data.Entities.ViewModels
+{
+    public class PersonViewModel
+    {
+    }
+}
