db1aa365c5c84b390b9db38feb623bf4ab61def7
Daniella Noren <daniellanoren@live.se>
2021-08-31 16:32:25 +0200

    Add methods to city and country repos

--- PeopleMVC/Data/DataManagement/Cities/DatabaseCityRepo.cs
+++ PeopleMVC/Data/DataManagement/Cities/DatabaseCityRepo.cs
@@ -1,4 +1,7 @@
-﻿using PeopleMVC.Data.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleMVC.Data.DataBase;
+using PeopleMVC.Data.Entities;
+using PeopleMVC.Models.DataManagement;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,24 +11,49 @@ namespace PeopleMVC.Data.DataManagement.Cities
 {
     public class DatabaseCityRepo : ICityRepo
     {
+        private readonly PeopleContext _context;
+
+        public DatabaseCityRepo(PeopleContext context)
+        {
+            _context = context;
+        }
         public City Create(int countryId, string cityName)
         {
-            throw new NotImplementedException();
+            return _context.Cities.Add(new City() { CountryId = countryId, Name = cityName }).Entity;
         }
 
         public bool Delete(City city)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.Cities.Remove(city);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateException e)
+            {
+                return false;
+            }
+        
+
+            return true;
         }
 
         public List<City> Read()
         {
-            throw new NotImplementedException();
+            return _context.Cities.Include(c => c.Country).ToList(); 
         }
 
         public City Read(int id)
         {
-            throw new NotImplementedException();
+            City city = _context.Cities.Find(id);
+
+            if(city == null)
+            {
+                throw new EntityNotFoundException("City with id " + id + " cannot be found");
+            }
+
+            return city;
+
         }
 
         public City Update(City city)
--- PeopleMVC/Data/DataManagement/Countries/DatabaseCountryRepo.cs
+++ PeopleMVC/Data/DataManagement/Countries/DatabaseCountryRepo.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeopleMVC.Data.DataBase;
 using PeopleMVC.Data.Entities;
 using System;
 using System.Collections.Generic;
@@ -9,14 +11,32 @@ namespace PeopleMVC.Data.DataManagement.Countries
 {
     public class DatabaseCountryRepo : ICountryRepo
     {
+        private readonly PeopleContext _context;
+
+        public DatabaseCountryRepo(PeopleContext context)
+        {
+            this._context = context;
+        }
         public Country Create(string countryName)
         {
-            throw new NotImplementedException();
+            Country country =_context.Countries.Add(new Country() { Name = countryName }).Entity;
+            _context.SaveChanges();
+            return country;
         }
 
         public bool Delete(Country country)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.Countries.Remove(country);
+                _context.SaveChanges();
+            }catch(DbUpdateException e)
+            {
+                return false;
+            }
+           
+
+            return true;
         }
 
         public List<Country> Read()
